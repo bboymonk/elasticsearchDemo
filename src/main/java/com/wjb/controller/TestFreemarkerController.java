e8fb0f5daea3142a1129b120e5426e6270fa55d1
mahongying <mahongying@jkwx.cn>
2017-07-04 15:17:26 +0800

    User

--- src/main/java/com/wjb/controller/TestFreemarkerController.java
+++ src/main/java/com/wjb/controller/TestFreemarkerController.java
@@ -4,14 +4,7 @@ import com.wjb.base.BaseController;
 import com.wjb.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by wjb on 2017/6/16.
---
+++ src/main/java/com/wjb/model/User.java
@@ -0,0 +1,43 @@
+package com.wjb.model;
+
+/**
+ * Created by Administrator on 2017/7/4.
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
